da892a27efdfb3da86030c148d38fb9481455b67
khanisha123 <khanisha@code.edu.az>
2022-01-25 01:12:38 +0400

    Add project files.

---
+++ FiorelloBack/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FiorelloBack.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FiorelloBack.DAL
+{
+    public class AppDbContext:IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options){}
+
+        public DbSet<Slider>Sliders { get; set; }
+        public DbSet<Flower>Flowers { get; set; }
+        public DbSet<FlowerImage>FlowerImages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<FlowerCategory> FlowerCategories { get; set; }
+        public DbSet<Campaign> Campaigns { get; set; }
+
+    }
+}
